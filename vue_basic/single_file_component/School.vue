3b17941a966fccfa1684c580d3a388d423b5ffa6
ntyaiqingr <tongyangni1228@gmail.com>
2022-05-06 12:50:27 +0800

    how to create single file vue component

---
+++ vue_basic/single_file_component/School.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="demo">
+        <h2>School Name: {{ schoolName }}</h2>
+        <h2>School Address: {{ schoolAddress}}</h2>
+        <button @click="showName">Click me to show school name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "School",
+        data() {
+            return {
+                schoolName: "yigao",
+                schoolAddress: "luoyang"
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.schoolName)
+            }
+        }
+    }
+    // 统一暴露
+    // export {school} 
+    // 默认暴露 *
+    // export default school
+</script>
+
+<style>
+    .demo{
+        background-color: aqua;
+    }
+</style>
---
+++ vue_basic/single_file_component/Student.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+        <h2>Student Name: {{ studentName }}</h2>
+        <h2>Student Age: {{ studentAge}}</h2>
+        <button @click="showName">Click me to show student name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Student",
+        data() {
+            return {
+                studentName: "Nick",
+                studentAge: 31
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.studentName)
+            }
+        }
+    }
+</script>
+
+<style>
+    .demo{
+        background-color: orange;
+    }
+</style>
